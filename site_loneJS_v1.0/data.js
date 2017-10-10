a8d09a3a86b8ce5b64411b2e9c6effe09e8312f6
Bob Main <bobmain49@gmail.com>
2017-10-10 12:25:08 -0700

    adding new file structure for loneJS version additions

---
+++ site_loneJS_v1.0/data.js
@@ -0,0 +1,17 @@
+var _DATA = {
+  home: {
+    title: "Welcome to LoneJS",
+  },
+  component: {
+    title: "Component Collection",
+  },
+  plugins: {
+    title: "Plugin Collection",
+  },
+  docs: {
+    title: "Documentation",
+  },
+  contact: {
+    title: "Contact",
+  },
+}
